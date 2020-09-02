18d1f8830c8b4dffe69c65f536a5404653d6799e
xatongxue <1120877019@qq.com>
2020-09-02 20:16:40 +0800

    没写完的

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,58 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="container">
+      <div
+        class="item"
+        :style="{ height: item * 10 + 'px', backgroundColor: colorList[index] }"
+        v-for="(item, index) in list"
+        :key="index"
+      >
+        {{ item }}
+      </div>
+    </div>
+    <button @click="changeSort">点击切换排序</button>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data () {
+    return {
+      list: [10, 8, 15, 6, 2],
+      colorList: ['red', 'green', 'blue', 'yellow', 'black'],
+      flag: false
+    }
+  },
+  methods: {
+    changeSort () {
+      function sortDec (a, b) {
+        return a - b
+      }
+      function sortAdd (a, b) {
+        return b - a
+      }
+      if (this.flag) {
+        this.list.sort(sortDec)
+        window.console.log(this.list)
+      } else {
+        this.list.sort(sortAdd)
+        window.console.log(this.list)
+      }
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.container {
+  display: flex;
+  align-items: start;
+  width: 500px;
+  height: 500px;
+  border: 1px solid #000;
+}
+.container .item {
+  width: 50px;
 }
 </style>
